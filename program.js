53a668a7fab439294c5d50e40613269cfdfa8c9d
KhadiD-008 <Dadayeva.Khadizha@mail.ru>
2025-03-25 19:46:35 +0300

    done

---
+++ program.js
@@ -0,0 +1,58 @@
+/* 
+let grade = prompt("Введите вашу оценку:");
+
+if (grade >= 90) {
+    console.log("Ваша оценка - A");
+} else if (grade >= 80 && grade <= 89) {
+    console.log("Ваша оценка - B");
+} else if (grade >= 70 && grade <= 79) {
+    console.log("Ваша оценка - C");
+} else {
+    console.log("Ваша оценка - F");
+}
+*/
+/*
+let number = prompt("Введите число:");
+
+for (let i=1; i<= 10; i++) {
+    console.log(number * i);
+    
+}
+*/
+/*
+let number = prompt("Введите число:");
+let sum = 0;
+
+for (let i=2; i <= number; i += 2) {
+    sum += i
+}
+console.log(sum);
+*/
+/*
+let operation = prompt('Выберите операцию: +, -, *, /');
+let num1 = parseFloat(prompt('Введите первое число:'));
+let num2 = parseFloat(prompt('Введите второе число:'));
+let result;
+
+ switch (operation) {
+        case '+':
+            result = num1 + num2;
+            break;
+        case '-':
+            result = num1 - num2;
+            break;
+        case '*':
+            result = num1 * num2;
+            break;
+        case '/':
+            if (num2 === 0) {
+                result = 'Ошибка: деление на ноль!';
+            } else {
+                result = num1 / num2;
+            }
+            break;
+        default:
+            result = 'Ошибка: неизвестная операция!';
+    }
+    alert(result);
+    */
